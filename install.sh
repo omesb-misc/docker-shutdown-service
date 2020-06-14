6257411bf188021af359a0d215dcaad26c1a3324
omesb <36600384+omesb@users.noreply.github.com>
2020-06-14 13:08:38 +0200

    install script update

--- install.sh
+++ install.sh
@@ -2,14 +2,17 @@
 
 echo "===> changing permissions";
 sudo chmod 755 stop-docker.sh stop-docker-containers.service;
+echo "DONE!"
 
 echo "===> copying the files";
 sudo cp stop-docker.sh /usr/local/bin; sudo cp stop-docker-containers.service /etc/systemd/system/;
+echo "DONE!"
 
 echo "===> restart systemd daemon:";
 sudo systemctl daemon-reload;
+echo "DONE!"
 
 echo "===> enable the service:";
 sudo systemctl enable --now stop-docker-containers.service;
 
-echo "DONE!"
+echo "Installation complete!"
